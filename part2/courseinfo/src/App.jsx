b08dcc07a8e7cfaab25cec9f730d11b29b248d44
lagecode <lagecode@outlook.com>
2025-01-29 16:10:41 +0100

    impl: part2 - 2.4 | Course Information step9, displaying course collection instead of single one

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,8 +1,4 @@
-const Header = ({ title }) => {
-  return (
-    <h2>{title}</h2>
-  )
-}
+const Header = ({ title }) => <h2>{title}</h2>
 
 const Part = ({ name, exercises }) => <li><p>{name} <sub>{exercises} exercises</sub></p></li>
 
@@ -20,30 +16,72 @@ const Course = ({ course }) => {
   )
 }
 
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+const CourseColl = ({ courses }) => {
+  return (
+    <div>
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
+        courses.map((course, i, arr) => {
+          return (
+            <>
+              <Course course={course} />
+              {
+                i !== arr.length - 1 && <hr />
+              }
+            </>
+          )
+        })
       }
-    ]
-  }
+    </div>
+  )
+}
 
-  return <Course course={course} />
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return <CourseColl courses={courses} />
 }
 
 export default App
